954e3bab409f56be1d9a16e8cb7403b4bb39ae0f
max-borisov <max.borisov@yahoo.com>
2015-04-23 17:53:42 +0300

    Create SignUpAsset component.

---
+++ frontend/assets/SignUpAsset.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace frontend\assets;
+
+class SignUpAsset extends BaseAsset
+{
+    public $css = [
+        'css/signup.css'
+    ];
+    public $js = [
+        'js/sign.up.js'
+    ];
+}
