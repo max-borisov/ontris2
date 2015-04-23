bb787d0ddf29170e45262f987d83a4dc1300a972
max-borisov <max.borisov@yahoo.com>
2015-04-23 17:51:40 +0300

    Update css files.

--- frontend/controllers/SessionController.php
+++ frontend/controllers/SessionController.php
@@ -3,7 +3,12 @@ namespace frontend\controllers;
 
 use Yii;
 use frontend\components\HelperUser;
-use frontend\models\LoginForm;
+use frontend\models\LogInForm;
+use frontend\models\SignUpForm;
+use frontend\models\UserAccountType;
+use frontend\models\CountryList;
+
+use frontend\components\Variable;
 
 /**
  * Session controller
@@ -11,6 +16,7 @@ use frontend\models\LoginForm;
 class SessionController extends AppController
 {
     // @todo Add action filter(login)
+    // @todo Add Session prefix for models
     public function actionIndex()
     {
         return $this->render('index');
@@ -21,7 +27,7 @@ class SessionController extends AppController
         if (!HelperUser::isGuest()) {
             return $this->goHome();
         }
-        $model = new LoginForm();
+        $model = new LogInForm();
         $formData = Yii::$app->request->post();
         // Set default value
         if (!isset($formData['rememberMe'])) {
@@ -30,7 +36,7 @@ class SessionController extends AppController
         if ($model->load($formData) && $model->login()) {
             return $this->goBack();
         } else {
-            return $this->render('login', [
+            return $this->render('logIn', [
                 'model' => $model,
             ]);
         }
@@ -38,6 +44,26 @@ class SessionController extends AppController
 
     public function actionSignup()
     {
-        return $this->render('signup');
+        $model = new SignUpForm();
+        /*if ($model->load(Yii::$app->request->post())) {
+            if ($user = $model->signup()) {
+                if (Yii::$app->getUser()->login($user)) {
+                    return $this->goHome();
+                }
+            }
+        }*/
+
+        // Get the list of possible user types (e.g. customer, transporter)
+        $accountList = (new UserAccountType)->getListBasedData();
+        // List of options where from a user know about the site
+//        $uFromList = UserComeFrom::model()->getListBasedData($this->appLanguage);
+        $countryList = (new CountryList)->getListBasedData();
+//        Variable::dump($countryList);
+
+        return $this->render('signUp/view', [
+            'model' => $model,
+            'countryList' => $countryList,
+            'accountList' => $accountList,
+        ]);
     }
 }
