4549e1577ca6ec2a02f0a1681fca506206838cc9
max-borisov <max.borisov@yahoo.com>
2015-04-24 10:27:10 +0300

    Update Sandbox controller

--- frontend/controllers/SandboxController.php
+++ frontend/controllers/SandboxController.php
@@ -4,11 +4,23 @@ namespace frontend\controllers;
 use Yii;
 use yii\web\Controller;
 use frontend\components\Variable;
+use frontend\models\MailQueue;
 
 class SandboxController extends Controller
 {
     public function actionIndex()
     {
-        Variable::dump(Yii::$app->homeUrl);
+//        Variable::dump(Yii::$app->homeUrl);
+//        Yii::error(['a' => '1', 'b' => 2], 'custom');
+    }
+
+    public function actionQueue()
+    {
+        $res = MailQueue::add(
+            'matt.borisov@gmail.com',
+            'Matt Borisov',
+            'Test subj',
+            'Hello boy', '<strong>Hello boy</strong>'
+        );
     }
 }
