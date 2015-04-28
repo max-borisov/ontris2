51aca07034a3795df9d761f1429ac9520c230786
max-borisov <max.borisov@yahoo.com>
2015-04-28 15:55:47 +0300

    Add signUpConfirmation messages

---
+++ common/mail/da/signUpConfirmation.php
@@ -0,0 +1,5 @@
+<p>Tak for din tilmelding, som vi for god ordens skyld lige skal bede dig bekræfte ved at klikke på dette link:</p>
+
+<p>{confUrl}</p>
+
+<p>Hvis dit mail program ikke understøtte denne funktion, kan adressen kopieres til adressefeltet i din internetbrowser.</p>
---
+++ common/mail/en/signUpConfirmation.php
@@ -0,0 +1,5 @@
+<p>Thanks for signing up, we for good measure just to ask you to confirm by clicking on this link:</p>
+
+<p>{confUrl}</p>
+
+<p>If your mail program does not support this feature, the address can be copied into the address bar of your internet browser.</p>
