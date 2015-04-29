b7c1ab018c0798715e7d7d768a22897eaf9a45e9
max-borisov <max.borisov@yahoo.com>
2015-04-29 16:25:35 +0300

    Update emails for sign-up

--- common/mail/da/signUpConfirmation.php
+++ common/mail/da/signUpConfirmation.php
@@ -1,5 +1,5 @@
 <p>Tak for din tilmelding, som vi for god ordens skyld lige skal bede dig bekræfte ved at klikke på dette link:</p>
 
-<p>{confUrl}</p>
+<p><a href="<?= $confirmationUrl ?>">Bekræft e-mail</a></p>
 
 <p>Hvis dit mail program ikke understøtte denne funktion, kan adressen kopieres til adressefeltet i din internetbrowser.</p>
--- common/mail/en/signUpConfirmation.php
+++ common/mail/en/signUpConfirmation.php
@@ -1,5 +1,5 @@
 <p>Thanks for signing up, we for good measure just to ask you to confirm by clicking on this link:</p>
 
-<p><?= $confirmationUrl ?></p>
+<p><a href="<?= $confirmationUrl ?>">Confirm email</a></p>
 
 <p>If your mail program does not support this feature, the address can be copied into the address bar of your internet browser.</p>
