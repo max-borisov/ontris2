d34ad34edf719638fda41b8321671d23bf5b19f4
max-borisov <max.borisov@yahoo.com>
2015-04-30 11:52:56 +0300

    Add new messages for email subjects.

--- common/messages/da-DA/email-subject.php
+++ common/messages/da-DA/email-subject.php
@@ -1,5 +1,6 @@
 <?php
 return [
     'signup_confirmation' => 'ONTRIS - Tilmelding',
+    'notify_admin_about_new_user' => 'ONTRIS - notification about a new user',
 ];
 
--- common/messages/en-US/email-subject.php
+++ common/messages/en-US/email-subject.php
@@ -1,5 +1,6 @@
 <?php
 return [
     'signup_confirmation' => 'ONTRIS - signup confirmation',
+    'notify_admin_about_new_user' => 'ONTRIS - notification about a new user',
 ];
 
